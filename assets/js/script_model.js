e0977d56f341592267273a5c9e52773c5e281830
Robson Oliveira Magalhães <robsonom92@hotmail.com>
2023-07-06 16:58:03 -0300

    funcionalidades

--- assets/js/script_model.js
+++ assets/js/script_model.js
@@ -33,12 +33,33 @@ document.getElementById('file-encarte').addEventListener('change', addEncarte);
 //     document.getElementById('display-encarte').style.display='none';
 // });
 
-function addBox(){
-    alert('adicionou');
+
+
+var valor = '';
+
+function addBox(event){
+    let count =0;
+    count+=1;
+    console.log(count)
+    let valor = parseFloat(count);
+    valor+=1;
+    console.log('Contagem: '+valor);
+    let pos = event.target.dataset.id;
+    console.log('Posição: '+pos);
+    let container = document.querySelectorAll('#view-encarte')[pos];
+    container.innerHTML +=
+    `<div id="encarte-main" class="view" >
+        <label for="file-encarte" onclick="addEncarte(event)" class="display-encarte"  data-id="${valor}">+</label>
+            <input type="file" id="file-encarte">
+    `;
+    return valor;
 }
 
-function removeBox(){
-    alert('removeu');
+function removeBox(event){
+    let pos = event.target.dataset.id;
+    console.log(pos);
+    let del = document.querySelectorAll('.view')[0];
+    del.remove();
 }
 
 
