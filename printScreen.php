39ede3c1967c7d49bf92c9ff95df03504a744e18
Robson Oliveira Magalhães <robsonom92@hotmail.com>
2023-07-12 14:47:17 -0300

    Update printScreen.php

--- printScreen.php
+++ printScreen.php
@@ -1,7 +1,9 @@
 <?php
 $params = http_build_query(array(
     "access_key" => "e7fd9d999f9c4ab2854f3aabcfff36ae",
-    "url" => "https://robson-magalhaes.github.io/Criar_encarte_basico/criarEncarte.html",
+    // "url" => "https://robson-magalhaes.github.io/Criar_encarte_basico/criarEncarte.html",
+    "format" => "jpeg",
+    "response_type"=>"image",
     "element"=> "main"
 ));
 
