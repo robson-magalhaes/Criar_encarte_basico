502040f4c7801c1608620c7693f86d0265724628
Robson Oliveira Magalhães <robsonom92@hotmail.com>
2023-07-12 14:30:09 -0300

    botao

--- printScreen.php
+++ printScreen.php
@@ -1,7 +1,7 @@
 <?php
 $params = http_build_query(array(
     "access_key" => "e7fd9d999f9c4ab2854f3aabcfff36ae",
-    "url" => "http://localhost/Criar_encarte/criarEncarte.html",
+    "url" => "https://robson-magalhaes.github.io/Criar_encarte_basico/criarEncarte.html",
     "element"=> "main"
 ));
 
