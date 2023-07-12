ebdf336c52e88e56aaee62453b1fae6e8c4b9b4a
Robson Oliveira Magalhães <robsonom92@hotmail.com>
2023-07-12 14:28:03 -0300

    adicionando API
    capturar tela

---
+++ printScreen.php
@@ -0,0 +1,11 @@
+<?php
+$params = http_build_query(array(
+    "access_key" => "e7fd9d999f9c4ab2854f3aabcfff36ae",
+    "url" => "http://localhost/Criar_encarte/criarEncarte.html",
+    "element"=> "main"
+));
+
+$image_data = file_get_contents("https://api.apiflash.com/v1/urltoimage?" . $params);
+file_put_contents("screenshot.jpeg", $image_data);
+
+header('Location:'.'criarEncarte.html');
